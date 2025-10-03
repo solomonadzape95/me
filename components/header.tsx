e932daa31dafb9ed571ea4493be0afcdafd81580
solomonadzape95 <adzapesolomon@gmail.com>
2025-10-03 14:03:58 +0100

    fix: worked on header layout for mobile

--- components/header.tsx
+++ components/header.tsx
@@ -16,8 +16,8 @@ export default function Header() {
     const isActive = (path: string) => pathname === path;
     return <header className={`flex items-center justify-between bg-background py-5 sticky top-0 left-0 right-0 z-50 font-mono`}>
         <section className="flex items-center justify-between w-11/12 mx-auto bg-transparent">
-            <span className="text-2xl flex items-center gap-2">
-                <span onClick={() => { router.push("/") }} className="cursor-pointer">~</span>{pathname === "/" ? "" : pathname} /<span className="w-3 h-6 inline-block bg-white animate-pulse"></span>
+            <span className="lg:text-2xl flex items-center gap-2">
+                <span onClick={() => { router.push("/") }} className="cursor-pointer">~</span>{pathname === "/" ? "" : pathname} /<span className="w-1 h-4 lg:w-3 lg:h-6 inline-block bg-white animate-pulse"></span>
             </span>
             <nav className="items-center gap-8 text-white text-2xl hidden lg:flex">
                 {links.map((link) => (
@@ -36,10 +36,10 @@ export default function Header() {
                     <span className="relative w-10 h-10 flex items-center justify-center">
                         {/* Animated morph between Bars3Icon and XMarkIcon */}
                         <Bars3Icon
-                            className={`w-10 h-10 absolute transition-all duration-300 ${isMobileNavOpen ? "opacity-0 scale-90 rotate-45" : "opacity-100 scale-100 rotate-0"}`}
+                            className={`w-7 h-7 absolute transition-all duration-300 ${isMobileNavOpen ? "opacity-0 scale-90 rotate-45" : "opacity-100 scale-100 rotate-0"}`}
                         />
                         <XMarkIcon
-                            className={`w-10 h-10 absolute transition-all duration-300 ${isMobileNavOpen ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-90 -rotate-45"}`}
+                            className={`w-7 h-7 absolute transition-all duration-300 ${isMobileNavOpen ? "opacity-100 scale-100 rotate-0" : "opacity-0 scale-90 -rotate-45"}`}
                         />
                     </span>
                 </button>
@@ -55,10 +55,10 @@ export default function Header() {
                         {links.map((link) => (
                             <span
                                 key={link.href}
-                                className={`h-[10vh] px-10 md:px-14 w-full cursor-pointer group relative flex items-center gap-0 hover:bg-gray-50/20 hover:text-white ${isActive(link.href) ? "text-white" : "text-gray-400"}`}
+                                className={`h-[10vh] pr-10 pl-4 md:px-14 w-full cursor-pointer group relative flex items-center gap-0 hover:bg-gray-50/20 hover:text-white ${isActive(link.href) ? "text-white" : "text-gray-400"}`}
                                 onClick={() => { router.push(link.href) }}
                             >
-                                <SlashIcon className="w-6 h-8 transition-transform duration-300 opacity-100 group-hover:opacity-0 group-hover:rotate-70 inline-block absolute left-10 md:left-14" />
+                                <SlashIcon className="w-6 h-8 transition-transform duration-300 opacity-100 group-hover:opacity-0 group-hover:rotate-70 inline-block absolute left-6 md:left-14" />
                                 <ArrowLongRightIcon className="w-6 h-8 transition-transform duration-300 -rotate-70 opacity-0 group-hover:inline-block group-hover:opacity-100 group-hover:rotate-0" />
                                 <span className="">{link.label}</span>
                             </span>
