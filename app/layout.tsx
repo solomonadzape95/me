aeb6baea0837233c4ce2934f56a86712c6e591dc
solomonadzape95 <adzapesolomon@gmail.com>
2025-10-03 18:38:20 +0100

    feat: added og links for sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="scrollbar-none">
       <head>
+
+      <meta property="og:title" content={`Solomon "solenoid" Adzape `} />
+      <meta property="og:description" content="Personal website for Solomon Adzape" />
+      <meta property="og:image" content="https://solenoid.me/images/file.jpg" />
+      <meta property="og:url" content="https://solenoid.me" />
+      <meta property="og:type" content="website" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="eec0ba25-604f-4991-ab62-eb9a1c7446f1"></script>
 
       </head>
